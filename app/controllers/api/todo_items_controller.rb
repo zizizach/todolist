54667226fdc38009feb755f87cfd58f2a4ae1a4a
Zach Ho <zach@wow.com>
2017-01-23 16:24:59 +0000

    API for To-do items, create, update and delete.

---
+++ app/controllers/api/todo_items_controller.rb
@@ -0,0 +1,56 @@
+class Api::TodoItemsController < ApplicationController
+    skip_before_filter :verify_authenticity_token
+    before_filter :find_todo_list
+    
+    def create
+        item = @list.todo_items.new(item_params)
+        if item.save
+            render status: 200, json: {
+                message: "To-do item created successfully",
+                todo_list: @list,
+                todo_item: item
+            }.to_json
+        else
+            render status: 500, json: {
+                message: "To-do item failed to be created",
+                todo_item: item
+            }.to_json
+        end
+    end
+    
+    def update
+        item = @list.todo_items.find(params[:id])
+        if item.update(item_params)
+            render status: 200, json: {
+                message: "To-do item updated successfully",
+                todo_list: @list,
+                todo_item: item
+            }.to_json
+        else
+            render status: 422, json: {
+                message: "Something went wrong",
+                todo_list: @list,
+                todo_item: item
+            }.to_json
+        end
+    end
+    
+    def destroy
+        item = @list.todo_items.find(params[:id])
+        item.destroy
+        render status:200, json: {
+            message: "To-do item deleted",
+            todo_list: @list,
+            todo_item: item
+        }.to_json
+    end
+    
+    private
+    def item_params
+        params.require("todo_item").permit("content")
+    end
+    
+    def find_todo_list
+        @list = TodoList.find(params[:todo_list_id])
+    end
+end
--- app/controllers/api/todo_lists_controller.rb
+++ app/controllers/api/todo_lists_controller.rb
@@ -6,23 +6,45 @@ class Api::TodoListsController < ApplicationController
     
     def show
         list = TodoList.find(params[:id])
-        render json: list
+        render json: list.as_json(include: :todo_items)
+    end
+    
+    def update
+        list = TodoList.find(params[:id])
+        if list.update(list_params)
+            render status: 200, json: {
+                message: "Successfully updated the To-do List",
+                todo_list: list
+            }.to_json
+        else
+            render status: 422, json: {
+                message: "List cannot be updated",
+                todo_list: list
+            }.to_json
+        end
     end
     
     def create
         list = TodoList.new(list_params)
         if list.save
-            render json: {
-                status: 200,
+            render status: 200, json: {
                 message: "Successfully created To-do List.",
                 todo_list: list
             }.to_json
         else
-            render json: {
-                status: 500,
+            render status: 422, json: {
                 errors: list.errors
             }.to_json
         end
+    end
+    
+    def destroy
+        list = TodoList.find(params[:id])
+        list.destroy
+        render status: 200, json: {
+            message: "Successfully deleted the To-do List.",
+            todo_list: list
+        }.to_json
     end
     
     private
